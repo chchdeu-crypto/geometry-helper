7d3a66e3d7a5fab57bf750e8620095c9f16203e7
chaim <chchdeu@gmail.com>
2026-06-25 10:21:47 +0300

    added_triangle

---
+++ math_and_geometry.py
@@ -0,0 +1,8 @@
+# triangle
+while user_options != 6:
+    match choose:
+        case 1:
+            base=input("enter base")
+            height=input("enter height")
+            result=base*height/2
+            print(f"formula: base * height / 2/n the area is {result}")
